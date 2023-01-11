3e1a0c2cbf300630e1b7bacf6039844f36e8130b
Rachitavya Sharma <rachitavya@gmail.com>
2023-01-11 14:29:12 +0530

    updated

--- Practice/zeroesToEnd.java
+++ Practice/zeroesToEnd.java
@@ -1,38 +1,26 @@
 import java.util.*;
 public class zeroesToEnd
 {
-    int[] deletion(int arr[],int x, int n)
-    {
-        for(int i=x;i<n-1;i++)
-        {
-            arr[i]=arr[i+1];
-        }
-        arr[n-1]=0;
-        return arr;
-    }
-
-    void show(int arr[],int n)
-    {
-        for(int i=0;i<n;i++)
-        System.out.print(arr[i]+" ");
-    }
     public static void main(String arg[])
     {
         zeroesToEnd ob=new zeroesToEnd();
         Scanner sc=new Scanner(System.in);
+
         int n=sc.nextInt();
         int arr[]=new int[n];
         for(int i=0;i<n;i++)
         arr[i]=sc.nextInt();
-        System.out.print("Initial array - ");
-        ob.show(arr,n);
-
-        for(int i=0;i<n;i++)
-            if(arr[i]==0)
-            arr=ob.deletion(arr,i,n);
+        int c=0;
+        for (int i = 0; i < n; i++)
+            if (arr[i] != 0)
+                arr[c++] = arr[i]; 
+  
+        while (c < n)
+            arr[c++] = 0;
         
-        System.out.print("\nModified array - ");
-        ob.show(arr, n);
+        for(int i=0;i<n;i++)
+        System.out.print(arr[i]+" ");
+
         sc.close();
     }
 }
