f9f3941225af83ca2299a925e86d3898b09c6770
Rachitavya Sharma <rachitavya@gmail.com>
2023-04-11 16:21:48 +0530

    RC

--- Practice/clargs.py
+++ Practice/clargs.py
@@ -1,2 +1,9 @@
+# To write a program that takes command line as argument and print sum of those arguments
+
 import sys
 print(sys.argv)
+print(len(sys.argv))
+sum=0
+for i in range(1,len(sys.argv)):
+    sum+=int(sys.argv[i])
+print('sum =',sum)
