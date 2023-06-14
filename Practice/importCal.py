c0acbf60e8793744b6e254db68eed3940ab4e10f
Rachitavya Sharma <rachitavya@gmail.com>
2023-06-14 12:17:16 +0530

    local module

---
+++ Practice/importCal.py
@@ -0,0 +1,8 @@
+import calculator
+a=int(input())
+b=int(input())
+
+print('Add=',calculator.add(a,b))
+print('Sub=',calculator.sub(a,b))
+print('Mul=',calculator.mul(a,b))
+print('Div=',calculator.div(a,b))
